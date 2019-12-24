6d4a66e077533f8268019fb0df66b12ef8210028
Jordan Miller <jordan.j.miller@live.com>
2019-12-23 22:35:23 -0800

    working on tokens, created testing client

---
+++ auth/token.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const db = require('../data/db-config');
+
+const secret = process.env.JWT_SECRET;
+
+async function generateToken(user){
+    const payload = {
+        subject: user.id,
+    };
+
+    const admin = await db('users as u')
+        .where({username: user.username})
+        .join('admins as a', 'u.id', 'a.user_id')
+        .first();
+    if(admin){
+        payload['admin'] = true;
+    }
+
+    const options = {
+        expiresIn: "24h"
+    };
+
+    return jwt.sign(payload, secret, options);
+}
+
+module.exports = {
+    secret,
+    generateToken
+}
