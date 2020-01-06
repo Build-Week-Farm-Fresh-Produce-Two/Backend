d621f1691e83bdffd979609752c2177647e5d4a0
Jordan Miller <jordan.j.miller@live.com>
2020-01-06 13:45:08 -0800

    created orders table

--- migrations/20200106124638_Orders.js
+++ migrations/20200106124638_Orders.js
@@ -1,8 +1,30 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('orders', tbl => {
+        tbl.increments();
+        tbl.integer('farmID', 255)
+            .notNullable(),
+        tbl.integer('customerID', 255)
+            .notNullable(),
+        tbl.integer('totalPrice', 255)
+            .notNullable(),
+        tbl.string('measurementType', 255)
+            .notNullable();
+        tbl.boolean('paymentStatus', 255)
+            .notNullable();
+        tbl.boolean('fulfillmentStatus', 255)
+            .notNullable();
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('orders');
 };
+
+//orders
+// id
+// int farmID required
+// int customerID required
+// int totalPrice required
+// bool paymentStatus required
+// bool fulfillmentStatus required 
