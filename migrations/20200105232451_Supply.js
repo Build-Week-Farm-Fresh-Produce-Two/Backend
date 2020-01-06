c08d7afeede51cbd7112739b76effa020f6a5a1a
Jordan Miller <jordan.j.miller@live.com>
2020-01-06 13:43:13 -0800

    created supply table

--- migrations/20200105232451_Supply.js
+++ migrations/20200105232451_Supply.js
@@ -1,8 +1,27 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('supply', tbl => {
+        tbl.increments();
+        tbl.integer('farmID', 255)
+            .notNullable(),
+        tbl.integer('productID', 255)
+            .notNullable(),
+        tbl.string('measurementType', 255)
+            .notNullable();
+        tbl.integer('quantity', 255)
+            .notNullable();
+        tbl.integer('price', 255)
+            .notNullable();
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('supply');
 };
+
+//supply
+// farmID required
+// productID required
+// string measurement type (pounds, ounces, etc) required
+// int quantity required
+// int price required
