b3bb8e384f82fd314f62a781618fd7518ebe2b69
Jordan Miller <jordan.j.miller@live.com>
2020-01-09 03:32:00 -0800

    fixing syntax error

--- data/db-model.js
+++ data/db-model.js
@@ -23,7 +23,7 @@ function findByMultiple(table, value1, value2){
 }
 
 function searchBy(table, columnName, value){
-    return db(table).whereRaw(`LOWER(${columnName}) LIKE %${value.toLowerCase()}%`);
+    return db(table).whereRaw(`LOWER(${columnName}) LIKE '%${value.toLowerCase()}%'`);
 }
 
 function add(table, row){
