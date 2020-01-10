5f18c3b6e11650eea26ab229f8b793bca5420517
Jordan Miller <jordan.j.miller@live.com>
2020-01-09 20:28:34 -0800

    Updating to match new data structures

--- seeds/06_orders.js
+++ seeds/06_orders.js
@@ -1,11 +1,11 @@
 
 exports.seed = function(knex) {
   return knex('orders').insert([
-    {farmID: 1, customerID: 1, totalPrice: 3.99, paymentStatus: true, fulfillmentStatus: true },
-    {farmID: 1, customerID: 3, totalPrice: 20, paymentStatus: true, fulfillmentStatus: false },
-    {farmID: 1, customerID: 2, totalPrice: 23, paymentStatus: true, fulfillmentStatus: false },
-    {farmID: 2, customerID: 3, totalPrice: 3.99, paymentStatus: true, fulfillmentStatus: false },
-    {farmID: 2, customerID: 3, totalPrice: 3.99, paymentStatus: true, fulfillmentStatus: false },
-    {farmID: 3, customerID: 2, totalPrice: 3.99, paymentStatus: true, fulfillmentStatus: false },
+    {farmID: 1, customerID: 1, farmName: 'best farm', customerName: 'jim best', totalPrice: 3.99, paymentStatus: true, fulfillmentStatus: true },
+    {farmID: 1, customerID: 3, farmName: 'best farm', customerName: 'jim worst', totalPrice: 20, paymentStatus: true, fulfillmentStatus: false },
+    {farmID: 1, customerID: 2, farmName: 'best farm', customerName: 'farmer fred', totalPrice: 23, paymentStatus: true, fulfillmentStatus: false },
+    {farmID: 2, customerID: 3, farmName: 'second best farm', customerName: 'jim worst', totalPrice: 3.99, paymentStatus: true, fulfillmentStatus: false },
+    {farmID: 2, customerID: 3, farmName: 'second best farm', customerName: 'jim worst', totalPrice: 3.99, paymentStatus: true, fulfillmentStatus: false },
+    {farmID: 3, customerID: 2, farmName: 'worst farm', customerName: 'farmer fred', totalPrice: 3.99, paymentStatus: true, fulfillmentStatus: false },
   ], 'id');
 };
--- seeds/07_ordered_products.js
+++ seeds/07_ordered_products.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   return knex('orderedProducts').insert([
-    {orderID: 1, supplyID: 1, quantity: 1},
-    {orderID: 1, supplyID: 1, quantity: 1},
-    {orderID: 1, supplyID: 1, quantity: 1},
-    {orderID: 1, supplyID: 1, quantity: 1},
-    {orderID: 1, supplyID: 1, quantity: 1},
-    {orderID: 1, supplyID: 1, quantity: 1},
-    {orderID: 1, supplyID: 1, quantity: 1},
-    {orderID: 1, supplyID: 1, quantity: 1},
+    {orderID: 1, supplyID: 1, productName: 'testProductName 1', productDescription: 'testDesc 1', purchasedMeasurementType: 'Pounds', purchasedQuantity: 700, purchasedPrice: 10.99 },
+    {orderID: 1, supplyID: 2, productName: 'testProductName 2', productDescription: 'testDesc 2', purchasedMeasurementType: 'Pounds', purchasedQuantity: 3, purchasedPrice: 3 },
+    {orderID: 1, supplyID: 3, productName: 'testProductName 3', productDescription: 'testDesc 3', purchasedMeasurementType: 'Pounds', purchasedQuantity: 3, purchasedPrice: 5 },
+    {orderID: 2, supplyID: 1, productName: 'testProductName 1', productDescription: 'testDesc 1', purchasedMeasurementType: 'Pounds', purchasedQuantity: 1, purchasedPrice: 1 },
+    {orderID: 3, supplyID: 1, productName: 'testProductName 1', productDescription: 'testDesc 1', purchasedMeasurementType: 'Pounds', purchasedQuantity: 1, purchasedPrice: 1 },
+    {orderID: 4, supplyID: 1, productName: 'testProductName 1', productDescription: 'testDesc 1', purchasedMeasurementType: 'Pounds', purchasedQuantity: 1, purchasedPrice: 1 },
+    {orderID: 5, supplyID: 1, productName: 'testProductName 1', productDescription: 'testDesc 1', purchasedMeasurementType: 'Pounds', purchasedQuantity: 1, purchasedPrice: 1 },
+    {orderID: 6, supplyID: 1, productName: 'testProductName 1', productDescription: 'testDesc 1', purchasedMeasurementType: 'Pounds', purchasedQuantity: 1, purchasedPrice: 1 },
   ], 'id');
 };
